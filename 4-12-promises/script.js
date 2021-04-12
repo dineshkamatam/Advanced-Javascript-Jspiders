35407be827fa4ab885c5fcf6767bbc3b1e800d9a
dineshkamatam <dineshkamatam25@gmail.com>
2021-04-12 16:36:33 +0530

    added promises

---
+++ 4-12-promises/script.js
@@ -0,0 +1,65 @@
+//promises
+
+// let promise = new Promise((resolve, reject) => {
+//   let isJavaScriptGoot = false;
+//   if (isJavaScriptGoot) {
+//     resolve("yes i love javascript!"); // resolve function for fullfilled
+//   } else {
+//     reject("i dont like javascript"); // reject function for rejected value
+//   }
+// });
+
+// //promise prototype methods
+// // then and catch
+
+// // promise.prototype.then(); //executing successfully completed value
+// // promise.prototype.catch(); //executing rejected value
+
+// promise.then((data) => {
+//   console.log(data); //resolve block
+// });
+
+// promise.catch((err) => {
+//   console.log(err); // reject block
+// });
+
+new Promise((resolve, reject) => {
+  let roomCleaned = false;
+  if (roomCleaned) {
+    resolve("yes room cleaned i will go to watch IPL match");
+  } else {
+    reject("opps i need to clean then i will watch IPL ");
+  }
+})
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+//Set timeout method
+
+window.setTimeout(() => {
+  var x = 10;
+  console.log(x);
+}, 5000);
+
+let y = 20;
+console.log(y);
+
+// setInterval method
+let v = 0;
+
+let interval = setInterval(() => {
+  if (v === 10) {
+    clearInterval(interval);
+  }
+  document.body.innerHTML = `<h1>${v++}</h1>`;
+}, 1000);
+
+// let stop = () => {
+//   if (v === 10) {
+
+//   }
+// };
+
+// stop();
+
+// let accessWebsite = new prmises
